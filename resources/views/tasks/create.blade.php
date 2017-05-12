640ab498b30effd677b556ffbc4b1d9452f96cd5
Kiyotaka Saito <kiyotaka-saitou@united.jp>
2017-05-12 00:00:17 +0000

    CRUD tasks

---
+++ resources/views/tasks/create.blade.php
@@ -0,0 +1,15 @@
+@extends('layouts.app')
+
+@section('content')
+
+    <h1>タスク作成ページ</h1>
+
+    {!! Form::model($task, ['route' => 'tasks.store']) !!}
+        {!! Form::label('content', 'タスク名：') !!} 
+        {!! Form::text('content') !!} 
+
+        {!! Form::submit('作成') !!} 
+
+    {!! Form::close() !!}
+
+@endsection
---
+++ resources/views/tasks/edit.blade.php
@@ -0,0 +1,15 @@
+@extends('layouts.app')
+
+@section('content')
+
+    <h1>id = {{ $task->id }} のタスク編集</h1>
+
+    {!! Form::model($task, ['route' => ['tasks.update', $task->id], 'method' => 'put']) !!}
+        {!! Form::label('content', 'タスク名：') !!} 
+        {!! Form::text('content') !!} 
+
+        {!! Form::submit('更新') !!} 
+
+    {!! Form::close() !!}
+
+@endsection
