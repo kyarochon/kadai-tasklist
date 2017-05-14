4f42591d5ea981e721964e7243f112c6b1d6957e
Kiyotaka Saito <kiyotaka-saitou@united.jp>
2017-05-14 14:41:19 +0000

    bootstrap design

--- resources/views/tasks/create.blade.php
+++ resources/views/tasks/create.blade.php
@@ -4,15 +4,22 @@
 
     <h1>タスク作成ページ</h1>
 
-    {!! Form::model($task, ['route' => 'tasks.store']) !!}
-        {!! Form::label('status', 'ステータス：') !!} 
-        {!! Form::text('status') !!} 
-
-        {!! Form::label('content', 'タスク名：') !!} 
-        {!! Form::text('content') !!} 
+    <div class="row">
+        <div class="col-xs-12 col-sm-offset-2 col-sm-8 col-md-offset-2 col-md-8 col-lg-offset-3 col-lg-6">
+            {!! Form::model($task, ['route' => 'tasks.store']) !!}
+                <div class="form-group">
+                    {!! Form::label('status', 'ステータス：') !!} 
+                    {!! Form::text('status', null, ['class' => 'form-control']) !!}
+                </div>
+                            
+                <div class="form-group">
+                    {!! Form::label('content', 'タスク名：') !!} 
+                    {!! Form::text('content', null, ['class' => 'form-control']) !!}
+                </div>
         
-        {!! Form::submit('作成') !!} 
-
-    {!! Form::close() !!}
-
+                {!! Form::submit('登録', ['class' => 'btn btn-primary']) !!} 
+            {!! Form::close() !!}
+        </div>
+    </div>
+            
 @endsection
