348cbe44cfccc50cac136dc32e065922cdc32948
Kiyotaka Saito <kiyotaka-saitou@united.jp>
2017-05-14 09:43:48 +0000

    add column

--- resources/views/tasks/create.blade.php
+++ resources/views/tasks/create.blade.php
@@ -5,9 +5,12 @@
     <h1>タスク作成ページ</h1>
 
     {!! Form::model($task, ['route' => 'tasks.store']) !!}
+        {!! Form::label('status', 'ステータス：') !!} 
+        {!! Form::text('status') !!} 
+
         {!! Form::label('content', 'タスク名：') !!} 
         {!! Form::text('content') !!} 
-
+        
         {!! Form::submit('作成') !!} 
 
     {!! Form::close() !!}
--- resources/views/tasks/edit.blade.php
+++ resources/views/tasks/edit.blade.php
@@ -5,6 +5,15 @@
     <h1>id = {{ $task->id }} のタスク編集</h1>
 
     {!! Form::model($task, ['route' => ['tasks.update', $task->id], 'method' => 'put']) !!}
+
+        {!! Form::label('status', 'ステータス：') !!} 
+        {!! Form::select('status', ['未完了' => '未完了', '完了済' => '完了済', 'キャンセル' => 'キャンセル']); !!}
+        
+
+        <!--{!! Form::text('status') !!} -->
+
+
+
         {!! Form::label('content', 'タスク名：') !!} 
         {!! Form::text('content') !!} 
 
